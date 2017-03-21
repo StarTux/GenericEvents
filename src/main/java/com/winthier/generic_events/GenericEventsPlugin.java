d59533feb972368f563fd30cb27e0059725443cb
StarTux <ths.loy@googlemail.com>
2017-03-21 13:17:30 +0100

    Command to retrieve item name

--- src/main/java/com/winthier/generic_events/GenericEventsPlugin.java
+++ src/main/java/com/winthier/generic_events/GenericEventsPlugin.java
@@ -2,6 +2,8 @@ package com.winthier.generic_events;
 
 import lombok.Getter;
 import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -18,6 +20,21 @@ public final class GenericEventsPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         instance = null;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = sender instanceof Player ? (Player)sender : null;
+        String cmd = args.length > 0 ? args[0].toLowerCase() : null;
+        if (cmd == null) {
+            return false;
+        } else if (cmd.equals("itemname")) {
+            if (player == null) return false;
+            sender.sendMessage("Item Name: '" + getItemName(player.getInventory().getItemInMainHand()) + "'");
+        } else {
+            return false;
+        }
+        return true;
     }
 
     public boolean playerCanBuild(Player player, Block block) {
